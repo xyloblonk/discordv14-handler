d7f6306e2537e4b30e2a2ce8d89f5ed2b0dc488d
xyloblonk <82177322+xyloblonk@users.noreply.github.com>
2025-04-07 23:53:23 -0700

    Update eventHandler.js

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,14 +1,34 @@
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
 const fs = require('fs');
 const path = require('path');
 const eventsPath = path.join(__dirname, '..', 'events');
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
 module.exports = (client) => {
   const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+  /*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
 
   for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
     const event = require(filePath);
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+    
     if (event.once) {
       client.once(event.name, (...args) => event.execute(...args, client));
     } else {
@@ -16,3 +36,8 @@ module.exports = (client) => {
     }
   }
 };
+
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
