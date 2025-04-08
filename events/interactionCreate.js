92d7526fb682bc1f83397863230459202186db2e
xyloblonk <82177322+xyloblonk@users.noreply.github.com>
2025-04-07 23:54:10 -0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,11 +1,26 @@
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction, client) {
     if (!interaction.isChatInputCommand()) return;
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+    
     const command = client.slashCommands.get(interaction.commandName);
     if (!command) return;
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+    
     try {
       await command.execute(interaction);
     } catch (error) {
@@ -14,3 +29,8 @@ module.exports = {
     }
   }
 };
+
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
