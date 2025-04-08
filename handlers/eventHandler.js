884488bc072dd29123c89b1dc8b6f43b801aeffa
xyloblonk <82177322+xyloblonk@users.noreply.github.com>
2025-04-07 23:46:10 -0700

    Create eventHandler.js

---
+++ handlers/eventHandler.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const eventsPath = path.join(__dirname, '..', 'events');
+
+module.exports = (client) => {
+  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+  for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args, client));
+    }
+  }
+};
