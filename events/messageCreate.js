bf7bafc9a808fe650ffb33e62835e208ab9e9499
xyloblonk <82177322+xyloblonk@users.noreply.github.com>
2025-04-07 23:54:31 -0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,15 +1,40 @@
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
 module.exports = {
   name: 'messageCreate',
   async execute(message, client) {
     if (message.author.bot || !message.content.startsWith(client.prefix)) return;
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+    
     const args = message.content.slice(client.prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+    
     const command = client.prefixCommands.get(commandName);
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+    
     if (!command) return;
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+    
     try {
       await command.execute(message, args, client);
     } catch (error) {
@@ -18,3 +43,8 @@ module.exports = {
     }
   }
 };
+
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
