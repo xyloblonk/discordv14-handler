43dad8cb276055f91514dd1071f0ec11c47fc5f1
xyloblonk <82177322+xyloblonk@users.noreply.github.com>
2025-04-07 23:56:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
+// TABLE OF CONTENTS
+// 1 Imports
+// 2 Setup the client
+// 3 Setup the client commands
+// 4 Import the handlers
+// 5 Login to the client
+
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
+// 1. Import needed dependencies
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token, prefix } = require('./config.json');
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
+// 2 Setup the client
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -9,12 +33,35 @@ const client = new Client({
   ]
 });
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
+// 3 Setup the client commands
 client.slashCommands = new Collection();
 client.prefixCommands = new Collection();
 client.prefix = prefix;
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
+// 4 Import the handlers
 require('./handlers/eventHandler')(client);
 require('./handlers/slashCommandHandler')(client);
 require('./handlers/prefixCommandHandler')(client);
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
+// 5 Login to the client
 client.login(token);
+
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
