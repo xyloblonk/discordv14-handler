2bacb6c3cf5a924ee83ecf8381bebc570483ec06
xyloblonk <82177322+xyloblonk@users.noreply.github.com>
2025-04-07 23:53:54 -0700

    Update slashCommandHandler.js

--- handlers/slashCommandHandler.js
+++ handlers/slashCommandHandler.js
@@ -1,9 +1,24 @@
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+
 const fs = require('fs');
 const path = require('path');
+
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
 
 module.exports = (client) => {
   const commandFiles = fs.readdirSync(path.join(__dirname, '..', 'slashCommands')).filter(file => file.endsWith('.js'));
 
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
+  
   for (const file of commandFiles) {
     const command = require(`../slashCommands/${file}`);
     if ('data' in command && 'execute' in command) {
@@ -13,3 +28,8 @@ module.exports = (client) => {
     }
   }
 };
+
+/*
+  MADE BY XYLOBLONK
+  https://github.com/xyloblonk/discordv14-handler
+*/
